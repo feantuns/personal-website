d9d1e0f4c0da2e03c1533842ad9c74bce3c8518b
Felipe Antunes <felipeantunes@MacBook-Air-de-Felipe.local>
2020-12-07 18:09:48 -0300

    blog list

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -1,34 +1,12 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
 import PostItem from "../components/PostItem"
 
-const IndexPage = () => {
-  const { allMarkdownRemark } = useStaticQuery(graphql`
-    query PostList {
-      allMarkdownRemark {
-        edges {
-          node {
-            frontmatter {
-              background
-              category
-              date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
-              description
-              title
-            }
-            timeToRead
-            fields {
-              slug
-            }
-          }
-        }
-      }
-    }
-  `)
-
-  const postList = allMarkdownRemark.edges
+const BlogList = props => {
+  const postList = props.data.allMarkdownRemark.edges
 
   return (
     <Layout>
@@ -56,4 +34,30 @@ const IndexPage = () => {
   )
 }
 
-export default IndexPage
+export const query = graphql`
+  query PostList($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      sort: { fields: frontmatter___date, order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
+      edges {
+        node {
+          frontmatter {
+            background
+            category
+            date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
+            description
+            title
+          }
+          timeToRead
+          fields {
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+export default BlogList
