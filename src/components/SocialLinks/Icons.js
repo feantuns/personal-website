52af1220d4c6d655deed63d92d87427980206d4d
Felipe Antunes <felipeantunes@MacBook-Air-de-Felipe.local>
2020-12-07 14:20:17 -0300

    sidebar + icons

---
+++ src/components/SocialLinks/Icons.js
@@ -0,0 +1,13 @@
+import { Github } from "@styled-icons/boxicons-logos/Github"
+import { Twitter } from "@styled-icons/boxicons-logos/Twitter"
+import { Instagram } from "@styled-icons/boxicons-logos/Instagram"
+import { Envelope } from "@styled-icons/boxicons-solid/Envelope"
+
+const Icons = {
+  Github,
+  Twitter,
+  Instagram,
+  Envelope,
+}
+
+export default Icons
---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,20 @@
+const links = [
+  {
+    label: "Github",
+    url: "https://github.com/felipeantuness",
+  },
+  {
+    label: "Twitter",
+    url: "https://twitter.com/ffelipeantunes",
+  },
+  {
+    label: "Instagram",
+    url: "https://www.instagram.com/ffelipeantuness",
+  },
+  {
+    label: "Envelope",
+    url: "mailto:contact@felipeantunes.me",
+  },
+]
+
+export default links
